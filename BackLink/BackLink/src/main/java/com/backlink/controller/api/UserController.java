0d6772a2236397f4b6298297a669b4003292f663
quoxank1796@gmail.com <quoxank1796@gmail.com>
2019-10-11 02:36:16 +0700

    update project

--- BackLink/BackLink/src/main/java/com/backlink/controller/api/UserController.java
+++ BackLink/BackLink/src/main/java/com/backlink/controller/api/UserController.java
@@ -1,10 +1,11 @@
 package com.backlink.controller.api;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,15 +22,21 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
+	@PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_CUSTOMER')")
+	@GetMapping("users/{id}")
+	public ResponseEntity<?> findOne(@PathVariable String id){
+		return new ResponseEntity<Object>(userService.getById(id), HttpStatus.OK);
+	}
+	
 	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	@GetMapping("users")
-	public List<User> findAll(){
-		return userService.findAll();
+	public ResponseEntity<?> findAll(){
+		return new ResponseEntity<Object>(userService.findAll(), HttpStatus.OK);
 	}
 	
 	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	@PostMapping("users")
-	public User createUser(@RequestBody User user) {
-		return userService.saveOne(user);
+	public ResponseEntity<?> createUser(@RequestBody User user) {
+		return new ResponseEntity<Object>(userService.saveOne(user), HttpStatus.OK);
 	}
 }
