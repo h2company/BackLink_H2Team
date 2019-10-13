f979f4c31f444f818905fce52efb0611f032c99d
quoxank1796@gmail.com <quoxank1796@gmail.com>
2019-10-13 18:58:19 +0700

    update

--- BackLink/BackLink/src/main/java/com/backlink/security/JwtAuthenticationEntryPoint.java
+++ BackLink/BackLink/src/main/java/com/backlink/security/JwtAuthenticationEntryPoint.java
@@ -22,7 +22,6 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 			AuthenticationException authException) throws IOException, ServletException {
 		logger.error("Responding with unauthorized error. Message - {}", authException.getMessage());
 		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
-
 	}
 
 }
